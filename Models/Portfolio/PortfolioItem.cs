1e03703da6a374eaea79d398ae1fa632aa5a5ecd
Pedro Martins <pedro.mourao.martins@example.com>
2024-07-16 13:40:09 +0100

    update solution edit portfolio Oreations item

--- Models/Portfolio/PortfolioItem.cs
+++ Models/Portfolio/PortfolioItem.cs
@@ -5,6 +5,8 @@ namespace AspnetCoreMvcFull.Models.Portfolio
     public int Id { get; set; }
     public int PortfolioId { get; set; }
     public string Symbol { get; set; }
+
+    public DateTime PurchaseDate { get; set; }
     public int Quantity { get; set; }
     public decimal PurchasePrice { get; set; }
     public decimal CurrentPrice { get; set; }
