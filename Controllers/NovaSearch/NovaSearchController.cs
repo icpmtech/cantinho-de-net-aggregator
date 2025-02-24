ee0267e32de77a1076bc6833827d124ee4ea7b1d
Pedro Martins <mourao.martins@gmail.com>
2025-02-24 16:23:14 +0000

    add

---
+++ Controllers/NovaSearch/NovaSearchController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketAnalyticHub.Controllers.NovaSearch
+{
+  public class NovaSearchController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+    public IActionResult Fynance()
+    {
+      return View();
+    }
+  }
+}
